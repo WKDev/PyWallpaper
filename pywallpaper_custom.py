0514fcdcf9030d2a6cb153d366ebcc7a500770e1
WKDev <chanhyukman@gmail.com>
2020-04-06 21:17:10 +0900

    0.1, smooth_change.py test

--- pywallpaper_custom.py
+++ pywallpaper_custom.py
@@ -1,5 +1,6 @@
 import os, sys, random, ctypes
 import time
+import cv2
 from PIL import Image
 
 import os
@@ -10,23 +11,30 @@ def get_wallpaper_label():
     wallpaper_list = os.listdir('./wallpaper/Catalina')
     return wallpaper_list
 
+
 # https://512pixels.net/projects/default-mac-wallpapers-in-5k/
 
-def change_wallpaper(wallpaper_list):
+def change_wallpaper(time, wallpaper_list):
     # print(wallpaper_list[1])
     # print(t.tm_hour % 3)
     # print('{}:{}'.format(t.tm_hour, t.tm_min))
 
-    'C:/Users/chanh/PyCharmProjects/PyWallpaper/wallpaper/Mojave/10-14-Mojave-15.jpg'
-    if (t.tm_sec == 58) and (t.tm_min == 0) and (t.tm_hour % 3 == 0):
-        print("정각이므로 시간을 바꿉니다.")
-        wallpaper_path = os.getcwd() + '\\wallpaper\\Catalina\\' + wallpaper_list[(t.tm_hour // 3)-1]
+    cur_pic = wallpaper_list[(t.tm_hour // 3) - 2]
+    prev_pic = wallpaper_list[(t.tm_hour // 3) - 1]
+
+    mix = 0
+    weighted_img = cv2.addWeighted(cur_pic, float(mix) / 100, prev_pic, float(100 - mix) / 100, 0)
+
+    # 'C:/Users/chanh/PyCharmProjects/PyWallpaper/wallpaper/Mojave/10-14-Mojave-15.jpg'
+    if (t.tm_sec % 30 ==0):
+        print("배경화면을 바꿉니다.")
+        # wallpaper_path = os.getcwd() + '\\wallpaper\\Catalina\\' + wallpaper_list[(t.tm_hour // 3)-1]
         # wallpaper_path.replace('\\', '/')
         # print(wallpaper_path, t.tm_hour // 3)
+        wallpaper_path = os.getcwd() + 'wallpaper.jpg'
         ctypes.windll.user32.SystemParametersInfoW(0x14, 0, wallpaper_path, 0x3)  # SystemParametersInfoA
     else:
         print('현재 시간 {}:{}'.format(t.tm_hour, t.tm_min))
-
 
 if __name__ == '__main__':
     list = get_wallpaper_label()
@@ -37,5 +45,5 @@ if __name__ == '__main__':
     while True:
         # icon.run()
         t = time.localtime()
-        
+
         change_wallpaper(list)
